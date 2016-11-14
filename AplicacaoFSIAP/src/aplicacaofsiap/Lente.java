a9ae82f7dee5c45166a7a260ff2d859d8d1de6f1
Helder Silva <1140808@isep.ipp.pt>
2016-11-14 19:14:05 +0000

    Classes Lente Polarizador e Analisador

---
+++ AplicacaoFSIAP/src/aplicacaofsiap/Lente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacaofsiap;
+
+/**
+ *
+ * @author Helder
+ */
+
+public abstract class Lente {
+    
+    private double angulo_emGraus;
+
+    public Lente(double angulo_emGraus) {
+        this.angulo_emGraus = angulo_emGraus;
+    }
+    
+    public double getAngulo_emGraus() {
+        return angulo_emGraus;
+    }
+
+    public void setAngulo_emGraus(double angulo_emGraus) {
+        this.angulo_emGraus = angulo_emGraus;
+    }
+
+    @Override
+    public String toString() {
+        return "Lente{" + "angulo_emGraus=" + angulo_emGraus + '}';
+    }
+
+}
