4ff40a9020129d0fdbba168a384741339747af9f
Helder Silva <1140808@isep.ipp.pt>
2016-11-17 23:27:26 +0000

    (cont.)

--- AplicacaoFSIAP/src/aplicacaofsiap/Absorcao/ClasseTesteAbsorcao.java
+++ AplicacaoFSIAP/src/aplicacaofsiap/Absorcao/ClasseTesteAbsorcao.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author Helder
  */
-public class ClasseTeste {
+public class ClasseTesteAbsorcao {
     
 
     public static void main(String[] args) {
--- AplicacaoFSIAP/src/ui/PAbsorcaoUI.java
+++ AplicacaoFSIAP/src/ui/PAbsorcaoUI.java
@@ -26,7 +26,8 @@ import javax.swing.border.TitledBorder;
  *
  * @author Pedro Fernandes
  */
-public class PAbsorcaoUI extends JDialog{
+public class PAbsorcaoUI extends JDialog {
+
     /**
      * Guarda a janela anterior
      */
@@ -34,12 +35,12 @@ public class PAbsorcaoUI extends JDialog{
     /**
      * Guarda a largura mínima da janela em píxeis.
      */
-    private static final int JANELA_LARGURA_MINIMO = 900, JANELA_ALTURA_MINIMO = 350; 
+    private static final int JANELA_LARGURA_MINIMO = 900, JANELA_ALTURA_MINIMO = 350;
     /**
      * Guarda a dimensão de uma label por omissão
      */
     private static final Dimension LABEL_TAMANHO = new JLabel("Intensidade:").
-                                                        getPreferredSize();
+            getPreferredSize();
     /**
      * Guarda os angulos e intensidades
      */
@@ -51,128 +52,133 @@ public class PAbsorcaoUI extends JDialog{
     /**
      * Guarda os botoes
      */
-    private JButton novaSimulacao,simular,voltar, guardar;
-    
+    private JButton novaSimulacao, simular, voltar, guardar;
+
     /**
-     * Constroi uma janela para simular polarizao por absorção
-     * recebendo a janela anterior como parametro
+     * Constroi uma janela para simular polarizao por absorção recebendo a
+     * janela anterior como parametro
+     *
      * @param framePai janela anterior
      */
-    public PAbsorcaoUI(DefinirPolarizaoUI framePai, Simulacao s){
+    public PAbsorcaoUI(DefinirPolarizaoUI framePai, Simulacao s) {
         super(framePai, "LIGHT GO -> Polarização por Absorção");
-        
+
         this.framePai = framePai;
-        this.s=s;
-        
+        this.s = s;
+
         criarComponentes();
-        
+
         setMinimumSize(new Dimension(JANELA_LARGURA_MINIMO, JANELA_ALTURA_MINIMO));
         setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         pack();
         setResizable(true);
         setLocationRelativeTo(framePai);
-        setVisible(true);        
+        setVisible(true);
+        setModal(true);
     }
-    
+
     /**
      * cria os componentes da janela
      */
-    private void criarComponentes(){        
-        
+    private void criarComponentes() {
+
         add(criarPainelGeral(), BorderLayout.NORTH);
         add(criarBotaoGuardar(), BorderLayout.CENTER);
-        
+
     }
-    
-    private JPanel criarPainelGeral(){        
+
+    private JPanel criarPainelGeral() {
         JPanel painel = new JPanel(new BorderLayout());
 
         painel.add(criarPainelResultados(), BorderLayout.NORTH);
         painel.add(criarPainelBotoes(), BorderLayout.CENTER);
-        
+
         return painel;
     }
-    private JPanel criarPainelResultados(){
+
+    private JPanel criarPainelResultados() {
         JPanel p = new JPanel(new GridLayout(1, 5));
 
-            p.add(criarPainelFeixeIncidente());
-            p.add(criarPainelPolarizador());
-            p.add(criarPainelFeixeIntermedio());
-            p.add(criarPainelAnalisador());
-            p.add(criarPainelFeixeResultante());
-            
-           return p;
+        p.add(criarPainelFeixeIncidente());
+        p.add(criarPainelPolarizador());
+        p.add(criarPainelFeixeIntermedio());
+        p.add(criarPainelAnalisador());
+        p.add(criarPainelFeixeResultante());
+
+        return p;
     }
-    private JPanel criarPainelFeixeIncidente(){
+
+    private JPanel criarPainelFeixeIncidente() {
         JPanel p = new JPanel();
-        
+
         p.setBorder(new TitledBorder("Feixe Incidente"));
-        
-        intTxt1=new JTextField(10);
-        
+
+        intTxt1 = new JTextField(10);
+
         p.add(criarPainelLabelTextfield("Intensidade", intTxt1));
-        
+
         return p;
     }
-    
-    private JPanel criarPainelPolarizador(){
+
+    private JPanel criarPainelPolarizador() {
         JPanel p = new JPanel();
-        
+
         p.setBorder(new TitledBorder("Polarizador"));
-        
-        angTxt1=new JTextField(10);
-        
+
+        angTxt1 = new JTextField(10);
+
         p.add(criarPainelLabelTextfield("Angulo", angTxt1));
-        
+
         return p;
     }
-    
-    private JPanel criarPainelFeixeIntermedio(){
+
+    private JPanel criarPainelFeixeIntermedio() {
         JPanel p = new JPanel();
-        
+
         p.setBorder(new TitledBorder("Feixe Intermedio"));
-        
-        intTxt2=new JTextField(10);
-        
+
+        intTxt2 = new JTextField(10);
+        intTxt2.setEditable(false);
         p.add(criarPainelLabelTextfield("Intensidade", intTxt2));
-        
+
         return p;
     }
-    
-    private JPanel criarPainelAnalisador(){
+
+    private JPanel criarPainelAnalisador() {
         JPanel p = new JPanel();
-        
+
         p.setBorder(new TitledBorder("Analisador"));
-        
-        angTxt2=new JTextField(10);
-        
+
+        angTxt2 = new JTextField(10);
+
         p.add(criarPainelLabelTextfield("Angulo", angTxt2));
-        
+
         return p;
     }
-    
-    private JPanel criarPainelFeixeResultante(){
+
+    private JPanel criarPainelFeixeResultante() {
         JPanel p = new JPanel();
-        
+
         p.setBorder(new TitledBorder("Feixe Resultante"));
-        
-        intTxt3=new JTextField(10);
-        
+
+        intTxt3 = new JTextField(10);
+        intTxt3.setEditable(false);
         p.add(criarPainelLabelTextfield("Intensidade", intTxt3));
-        
+
         return p;
     }
-    
+
     /**
      * cria painel para introduzir label1, campo para introdução dados
+     *
      * @param label1 introduzir label1
-     * @param texto campo para introdução dados 
+     * @param texto campo para introdução dados
      * @return painel para introduzir label1, campo para introdução dados
      */
     private JPanel criarPainelLabelTextfield(String label1, JTextField texto) {
         JLabel lb1 = new JLabel(label1, JLabel.RIGHT);
         lb1.setPreferredSize(LABEL_TAMANHO);
-        
+
         JPanel p = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
         p.add(lb1);
@@ -180,9 +186,10 @@ public class PAbsorcaoUI extends JDialog{
 
         return p;
     }
-    
+
     /**
      * cria painel botoes
+     *
      * @return painel botoes
      */
     private JPanel criarPainelBotoes() {
@@ -197,49 +204,137 @@ public class PAbsorcaoUI extends JDialog{
         p.add(criarBotaoSimular());
         p.add(criarBotaoNovaSimulacao());
         p.add(criarBotaoVoltar());
-        
+
         getRootPane().setDefaultButton(simular);
-        
+
         return p;
     }
-    
+
     /**
-     * criar botão simular 
-     * @return botão simular  
+     * criar botão simular
+     *
+     * @return botão simular
      */
     private JButton criarBotaoSimular() {
-        simular= new JButton("Simular");
+        simular = new JButton("Simular");
         simular.setMnemonic(KeyEvent.VK_S);
-        simular.setToolTipText("Simulacao a polarizacao por absorção");
+        simular.setToolTipText("Simular a polarizacao por absorção");
+
         simular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //implementar             
+                double int_feix_incid = 0;
+                double ang_polarizador = 0;
+
+                if (intTxt1.getText().isEmpty()) { //se nenhum valor tiver sido inserido
+                    popUpMensagemDErro("Insira um valor para a intensidade do feixe incidente!");
+                    intTxt1.requestFocus();
+
+                } else {  //foi inserido valor
+                    try {
+                        int_feix_incid = Double.parseDouble(intTxt1.getText());
+//                    } catch (NumberFormatException ex) { //se não foi introduzido um número
+//                        popUpMensagemDErro("A intensidade do feixe incidente deve ser um valor!");
+//                        intTxt1.requestFocus();
+//                    }
+                        //valor inserido é numérico
+                        if (int_feix_incid < 0) {
+                            popUpMensagemDErro("A intensidade do feixe incidente deve ser um valor positivo!");
+                            intTxt1.requestFocus();
+                        } else { //se valor de intensidade está correto vai validar ang_polarizador
+
+//---------------------validar angulo do polarizador--------------                       
+                            if (angTxt1.getText().isEmpty()) { //se nenhum valor tiver sido inserido
+                                popUpMensagemDErro("Insira um valor para o ângulo do polarizador.");
+                                angTxt1.requestFocus();
+
+                            } else {  //foi inserido valor
+                                try {
+                                    ang_polarizador = Double.parseDouble(angTxt1.getText());
+//                                } catch (NumberFormatException ex) { //se não foi introduzido um número
+//                                    popUpMensagemDErro("O ângulo do polarizador deve ser um valor!");
+//                                    angTxt1.requestFocus();
+//                                }
+                                    //valor inserido é numérico
+                                    if (ang_polarizador < 0 || ang_polarizador > 360) {
+                                        popUpMensagemDErro("O ângulo do polarizador deve ser um valor no intervalo [0-360].");
+                                        angTxt1.requestFocus();
+
+                                    } else { //se valor de intensidade está correto vai validar ang_polarizador
+//
+////---------------------validar angulo do polarizador--------------                       
+//                            if (angTxt1.getText().isEmpty()) { //se nenhum valor tiver sido inserido
+//                                popUpMensagemDErro("Insira um valor para o ângulo do polarizador.");
+//                                angTxt1.requestFocus();
+//
+//                            } else {  //foi inserido valor
+//                                try {
+//                                    ang_polarizador = Double.parseDouble(angTxt1.getText());
+////                                } catch (NumberFormatException ex) { //se não foi introduzido um número
+////                                    popUpMensagemDErro("O ângulo do polarizador deve ser um valor!");
+////                                    angTxt1.requestFocus();
+////                                }
+//                                    //valor inserido é numérico
+//                                    if (ang_polarizador < 0 || ang_polarizador > 360) {
+//                                        popUpMensagemDErro("O ângulo do polarizador deve ser um valor no intervalo [0-360].");
+//                                        angTxt1.requestFocus();
+
+                                    }
+
+                                } catch (NumberFormatException ex) { //se não foi introduzido um número
+                                    popUpMensagemDErro("O ângulo do polarizador deve ser um valor!");
+                                    angTxt1.requestFocus();
+                                }
+                            }
+
+                        } //fecho ult if intensidade
+
+                    } catch (NumberFormatException ex) { //se não foi introduzido um número
+                        popUpMensagemDErro("A intensidade do feixe incidente deve ser um valor!");
+                        intTxt1.requestFocus();
+                    }
+                } //fecho else
             }
         });
 
         return simular;
     }
+
+    private void popUpMensagemDErro(String msg) {
+        JOptionPane.showMessageDialog(framePai,
+                msg, "Dados da Simulação", JOptionPane.WARNING_MESSAGE);
+    }
+
+    private void validar_DadosDCampo(String fieldName, String nome_campo_a_validar) {
+
+    }
+
     /**
      * criar botão nova simulacao
+     *
      * @return botão nova simulacao
      */
     private JButton criarBotaoNovaSimulacao() {
-        novaSimulacao= new JButton("Nova Simulação");
+        novaSimulacao = new JButton("Nova Simulação");
         novaSimulacao.setMnemonic(KeyEvent.VK_N);
         novaSimulacao.setToolTipText("Nova Simulacao - polarizacao por absorção");
         novaSimulacao.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //implementar
+                intTxt1.setText("");
+                intTxt2.setText("");
+                intTxt3.setText("");
+                angTxt1.setText("");
+                angTxt2.setText("");
             }
         });
-
         return novaSimulacao;
     }
+
     /**
      * criar botão Voltar e volta menu anterior
-     * @return botão Voltar e volta menu anterior 
+     *
+     * @return botão Voltar e volta menu anterior
      */
     private JButton criarBotaoVoltar() {
         voltar = new JButton("Voltar");
@@ -251,12 +346,12 @@ public class PAbsorcaoUI extends JDialog{
                 dispose();
             }
         });
-
         return voltar;
     }
-    
+
     /**
      * criar botão Guardar
+     *
      * @return botão Guardar
      */
     private JButton criarBotaoGuardar() {
