68fd0ab013ea6fd0c9b8b7f078137d6494f171ae
Diana Silva <1151088@isep.ipp.pt>
2016-11-19 17:45:37 +0000

    Test- feixeDLuzIncident removido

--- AplicacaoFSIAP/test/aplicacaofsiap/FeixeDeLuzIncidenteTest.java
+++ AplicacaoFSIAP/test/aplicacaofsiap/FeixeDeLuzIncidenteTest.java
@@ -37,72 +37,73 @@ public class FeixeDeLuzIncidenteTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of getTipo method, of class FeixeDeLuzIncidente.
-     */
-    @Test
-    public void testGetTipo() {
-        System.out.println("getTipo");
-        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
-        String expResult = "não definido";
-        String result = instance.getTipo();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getAnguloDeIncidencia method, of class FeixeDeLuzIncidente.
-     */
-    @Test
-    public void testGetAnguloDeIncidencia() {
-        System.out.println("getAnguloDeIncidencia");
-        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
-        double expResult = -1000.0;
-        double result = instance.getAnguloDeIncidencia();
-        assertEquals(expResult, result, 0.0);
-    }
-
-    /**
-     * Test of setAngulo method, of class FeixeDeLuzIncidente.
-     */
-    @Test
-    public void testSetAngulo() {
-        System.out.println("setAngulo");
-        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
-        double anguloDeIncidencia=4;
-        boolean expResult = true;
-        boolean result = instance.setAngulo(anguloDeIncidencia);
-        assertEquals(expResult, result);
-
-    }
-    
-
-    /**
-     * Test of validaAngulo method, of class FeixeDeLuzIncidente.
-     */
-    @Test
-    public void testValidaAngulo() {
-        System.out.println("validaAngulo");
-        double angulo = 400;
-        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
-        boolean expResult = false;
-        boolean result = instance.validaAngulo(angulo);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of equals method, of class FeixeDeLuzIncidente.
-     */
-    @Test
-    public void testEquals() {
-        System.out.println("equals");
-        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
-        FeixeDeLuzIncidente outroFeixe = new FeixeDeLuzIncidente();
-        instance.setAngulo(100);
-        outroFeixe.setAngulo(100);
-
-        boolean expResult = true;
-        boolean result = instance.equals(outroFeixe);
-        assertEquals(expResult, result);
-    }
+//    /**
+//     * Test of getTipo method, of class FeixeDeLuzIncidente.
+//     */
+//    @Test
+//    public void testGetTipo() {
+//        System.out.println("getTipo");
+//        FeixeDLuzIncidente instance = new FeixeDLuzIncidente();
+//        super.
+//        String expResult = "não definido";
+//        String result = instance.getTipo();
+//        assertEquals(expResult, result);
+//    }
+//
+//    /**
+//     * Test of getAnguloDeIncidencia method, of class FeixeDeLuzIncidente.
+//     */
+//    @Test
+//    public void testGetAnguloDeIncidencia() {
+//        System.out.println("getAnguloDeIncidencia");
+//        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
+//        double expResult = -1000.0;
+//        double result = instance.getAnguloDeIncidencia();
+//        assertEquals(expResult, result, 0.0);
+//    }
+//
+//    /**
+//     * Test of setAngulo method, of class FeixeDeLuzIncidente.
+//     */
+//    @Test
+//    public void testSetAngulo() {
+//        System.out.println("setAngulo");
+//        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
+//        double anguloDeIncidencia=4;
+//        boolean expResult = true;
+//        boolean result = instance.setAngulo(anguloDeIncidencia);
+//        assertEquals(expResult, result);
+//
+//    }
+//    
+//
+//    /**
+//     * Test of validaAngulo method, of class FeixeDeLuzIncidente.
+//     */
+//    @Test
+//    public void testValidaAngulo() {
+//        System.out.println("validaAngulo");
+//        double angulo = 400;
+//        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
+//        boolean expResult = false;
+//        boolean result = instance.validaAngulo(angulo);
+//        assertEquals(expResult, result);
+//    }
+//
+//    /**
+//     * Test of equals method, of class FeixeDeLuzIncidente.
+//     */
+//    @Test
+//    public void testEquals() {
+//        System.out.println("equals");
+//        FeixeDeLuzIncidente instance = new FeixeDeLuzIncidente();
+//        FeixeDeLuzIncidente outroFeixe = new FeixeDeLuzIncidente();
+//        instance.setAngulo(100);
+//        outroFeixe.setAngulo(100);
+//
+//        boolean expResult = true;
+//        boolean result = instance.equals(outroFeixe);
+//        assertEquals(expResult, result);
+//    }
   
 }
